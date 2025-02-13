d44ac21c60acb4d1cc87306459b7ceeb85c7a6e2
Julian Diaz <julianmatiasdiaz6@gmail.com>
2025-02-13 09:54:15 -0300

    cambios webs en rama test

---
+++ js/pop-up.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var videoModal = document.getElementById("videoModal");
+    var iframe = document.getElementById("youtube-video");
+    var videoUrl = "https://www.youtube.com/embed/23eP8YVRXyc?list=PLvDyxtKlIcng7KjH451XcRAVGXiCnEvd6&autoplay=1&rel=0";
+
+    videoModal.addEventListener("show.bs.modal", function () {
+        iframe.src = videoUrl;
+    });
+
+    videoModal.addEventListener("hide.bs.modal", function () {
+        iframe.src = ""; // Detener el video cuando se cierre el modal
+    });
+});
